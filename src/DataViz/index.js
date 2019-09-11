1b08e5b1d507100893a3988ff88b207a0afaabfc
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2019-09-11 06:43:07 -0500

    Added sidebar to visualization page

--- src/DataViz/index.js
+++ src/DataViz/index.js
@@ -13,7 +13,7 @@ import FileDropdown2 from '../FileDropdown2'
 import VisualizationDropdown from '../VisualizationDropdown'
 import VisualizationDropdown2 from '../VisualizationDropdown2'
 
-import { Menu } from 'semantic-ui-react'
+import { Menu, Button, Icon, Sidebar, Segment } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 
@@ -26,7 +26,8 @@ class DataViz extends React.Component {
       selectedFileIndex: 0,
       selectedFileIndex2: 0,
       selectedVisualization: "BarChart",
-      selectedVisualization2: "BarChart"
+      selectedVisualization2: "BarChart",
+      visible: false
     }
   }
 
@@ -85,6 +86,12 @@ class DataViz extends React.Component {
     })
   }
 
+   toggleVisible = () => {
+    this.setState({
+        visible: !this.state.visible
+    })
+  }
+
   render(props){
     // Initialize variables to store selected visualizations
     let visualization;
@@ -107,56 +114,89 @@ class DataViz extends React.Component {
     }
 
     return(
-      //console.log(props, "HERE ARE PROPS IN DATA VIZ")
+      
       <div>
-          <Menu pointing secondary vertical>
-            <Menu.Item as={ Link } to="/">Switch User</Menu.Item>
-            <Menu.Item as={ Link } to="/dataviz">Data Visualization</Menu.Item>
-            <Menu.Item as={ Link } to="/register">Create New User</Menu.Item>
-            <Menu.Item onClick={this.handleLogOut}>Logout</Menu.Item>
-          </Menu>
+         
+        <Menu inverted fixed="top">
+                <Button className="item" onClick={this.toggleVisible}>
+                  <i className="sidebar icon" />
+                </Button>
+               <Menu.Item>
+                <img src='https://media.giphy.com/media/l41m3025MFqt6xXuo/giphy.gif' style={{width: '50px', border: '1px solid'}} />
+               </Menu.Item>
+               <Menu.Item style={{float: 'right'}} name="home" as={Link} to="/home">
+                  <Icon name="home" />
+               </Menu.Item>
+            </Menu>
+            
+            <Sidebar.Pushable as={Segment}>
+               
+               <Sidebar
+                visible={this.state.visible}
+                as={Menu}
+                animation="push"
+                width="thin"
+                icon="labeled"
+                vertical
+                inverted
+              >
+                <Menu.Item style={{height: '40px'}}></Menu.Item>
+                <Menu.Item style={{textAlign: 'center'}} name="login" as={Link} to="/">
+                  <Icon style={{float: 'left'}} name="user circle outline" />Switch User
+                </Menu.Item>
+                <Menu.Item style={{textAlign: 'center'}} name="home" as={Link} to="/files">
+                  <Icon style={{float: 'left'}} name="folder outline" />Upload Files
+                </Menu.Item>
+                <Menu.Item style={{textAlign: 'center'}} name="home" as={Link} to="/dataviz">
+                  <Icon style={{float: 'left'}} name="area graph" />Data Visualization
+                </Menu.Item>
+                <Menu.Item style={{textAlign: 'center'}} name="home" as={Link} to="/register">
+                  <Icon style={{float: 'left'}} name="cog" />Create New USer
+                </Menu.Item>
+              </Sidebar>
+                  <div style={{height: "75px"}}></div>
+                    <div style={{display: 'flex', flexDirection: 'column'}}>
 
+                        <div>
+                          <h1>Here is where the vizualizations will go</h1>
+                        </div>
 
-
-
-        <div style={{display: 'flex', flexDirection: 'column'}}>
-
-              <div>
-                <h1>Here is where the vizualizations will go</h1>
-              </div>
-
-          
-              <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
-                  <div>
-                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
-                      <VisualizationDropdown 
-                        visualizations={this.state.visualizationTypes} 
-                        selectVisualization={this.selectVisualization} 
-                      />
-                      <FileDropdown 
-                        files={this.state.data} 
-                        selectFile={this.selectFile} 
-                        />
-                    </div>
-                    {this.state.data === undefined || this.state.data.length === 0 || this.state.selectedFileIndex === undefined ? null : visualization}
-                  </div>
-                  <div>
-                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
-                      <VisualizationDropdown2 
-                        visualizations={this.state.visualizationTypes} 
-                        selectVisualization={this.selectVisualization2} 
-                      />
-                      <FileDropdown2 
-                        files={this.state.data} 
-                        selectFile2={this.selectFile2} 
-                      />
-                    </div>
-                    {this.state.data === undefined || this.state.data.length === 0 || this.state.selectedFileIndex2 === undefined ? null : visualization2}
-                  </div>
-              </div>
-
-        </div>  
-
+                        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+                            <div>
+                              <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+                                <VisualizationDropdown 
+                                  visualizations={this.state.visualizationTypes} 
+                                  selectVisualization={this.selectVisualization} 
+                                />
+                                <FileDropdown 
+                                  files={this.state.data} 
+                                  selectFile={this.selectFile} 
+                                  />
+                              </div>
+                              {this.state.data === undefined || this.state.data.length === 0 || this.state.selectedFileIndex === undefined ? null : visualization}
+                            </div>
+                            <div>
+                              <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+                                <VisualizationDropdown2 
+                                  visualizations={this.state.visualizationTypes} 
+                                  selectVisualization={this.selectVisualization2} 
+                                />
+                                <FileDropdown2 
+                                  files={this.state.data} 
+                                  selectFile2={this.selectFile2} 
+                                />
+                              </div>
+                              {this.state.data === undefined || this.state.data.length === 0 || this.state.selectedFileIndex2 === undefined ? null : visualization2}
+                            </div>
+                        </div>
+                  </div>  
+              <Sidebar.Pusher>
+                <Segment basic style={{height: '100vh'}}>
+                    <div style={{height: "50px"}}></div>
+                        
+                </Segment>
+              </Sidebar.Pusher>
+            </Sidebar.Pushable> 
      </div>
     )
   }
@@ -165,52 +205,5 @@ class DataViz extends React.Component {
 export default DataViz
 
 
-//<Menu pointing secondary vertical>
-           // <Menu.Item as={ Link } to="/">Switch User</Menu.Item>
-            //<Menu.Item as={ Link } to="/dataviz">Data Visualization</Menu.Item>
-            //<Menu.Item as={ Link } to="/register">Create New User</Menu.Item>
-            //<Menu.Item >Logout</Menu.Item>
-         // </Menu>
-
-
-// <div>
-
-//         <div style={{display: 'flex', flexDirection: 'column'}}>
-
-//           <div>
-//             <h1>Here is where the vizualizations will go</h1>
-//           </div>
-
-          
-//           <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
-//               <div>
-//                 <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
-//                   <VisualizationDropdown 
-//                     visualizations={this.state.visualizationTypes} 
-//                     selectVisualization={this.selectVisualization} 
-//                   />
-//                   <FileDropdown 
-//                     files={this.state.data} 
-//                     selectFile={this.selectFile} 
-//                     />
-//                 </div>
-//                 {this.state.data === undefined || this.state.data.length === 0 || this.state.selectedFileIndex === undefined ? null : visualization}
-//               </div>
-//               <div>
-//                 <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
-//                   <VisualizationDropdown2 
-//                     visualizations={this.state.visualizationTypes} 
-//                     selectVisualization={this.selectVisualization2} 
-//                   />
-//                   <FileDropdown2 
-//                     files={this.state.data} 
-//                     selectFile2={this.selectFile2} 
-//                   />
-//                 </div>
-//                 {this.state.data === undefined || this.state.data.length === 0 || this.state.selectedFileIndex2 === undefined ? null : visualization2}
-//               </div>
-//           </div>
-
-//         </div>  
 
 //      </div>
